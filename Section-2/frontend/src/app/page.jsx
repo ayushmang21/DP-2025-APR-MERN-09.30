d26ff7095f3ec55096065b2657463b825eb99069
Ayushman Gupta <ayushman0021@gmail.com>
2025-06-11 11:24:09 +0530

    Add Btn and Card components; implement Login and Signup pages

--- Section-2/frontend/src/app/page.jsx
+++ Section-2/frontend/src/app/page.jsx
@@ -1,9 +1,18 @@
+import Btn from '@/components/Btn';
+import Card from '@/components/Card';
 import React from 'react'
 
 const page = () => {
   return (
-    <div className='text-9xl text-amber-600' >Hello World</div>
+    <div className='bg-black/0'>
+      <div className='text-9xl text-amber-600' >Hello World</div>
+
+      <Btn />
+      <Card title= "ABc" description="ffughhih" />
+      <Card title= "ABdjqwjdowqjdojc" description="fewfwefwefewfewfih" />
+
+    </div>
   )
 }
 
-export default page
+export default page;
